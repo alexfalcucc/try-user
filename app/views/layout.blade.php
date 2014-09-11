770653ab5ee877b88c238954b1ff87b72774ec13
alexfalcucc <alex.falcucci@hotmail.com>
2014-09-11 10:55:43 -0300

    try-user-new

---
+++ app/views/layout.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>try-users!</title>
+	<link rel="stylesheet" href="">
+</head>
+<body>
+	<h1>Try-User is a simple test for Laravel. Development by Alex Falcucci.</h1>
+	@yield('content')
+</body>
+</html>
